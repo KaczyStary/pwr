e11563bcb1bfd41f64eb4e9cd85a53ebceb1fac3
GKuczek <grzegorz.k91@gmail.com>
2024-06-05 00:56:16 +0200

    [GrKu] task2-solution

--- src/main/java/com/convista/pwr/bc/StoreService.java
+++ src/main/java/com/convista/pwr/bc/StoreService.java
@@ -6,6 +6,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class StoreService {
 
+    // No magic numbers and strings !
+    private static final int ALCOHOL_LEGAL_AGE = 18;
+    private static final String ILLEGAL_MESSAGE = "It is illegal";
+    private static final String BLIK_PAYMENT_ERROR_MESSAGE = "Blik payment error";
+    private static final String BEER_BRAND = "Harnaś";
+
     @Autowired
     private PersonService personService;
 
@@ -13,8 +19,30 @@ public class StoreService {
     private CashService cashService;
 
     public Beer buyBeerForClient(Person client) {
-        // TODO: implement age restriction and use also dummy cashService to pay by blik
-        // TODO: if person is not allowed to: age < 18 or Blikpayment returns false throw Runtime exception with detailed message
-        return new Beer("Harnaś");
+
+        if (personService.calculateCurrentAge(client) >= ALCOHOL_LEGAL_AGE) {
+            if (cashService.payByBlik(123456)) {
+                return new Beer("Harnaś");
+            }
+            throw new RuntimeException("Blik payment error");
+        }
+        throw new RuntimeException("It is illegal");
     }
+
+    /**
+     * Jeden poziom 'wcięcia' brak zagnieżdżania bardziej czytelne warunki
+     *
+     */
+    public Beer buyBeerForClient2(Person client) {
+
+        if (personService.calculateCurrentAge(client) < ALCOHOL_LEGAL_AGE) {
+            throw new RuntimeException(ILLEGAL_MESSAGE);
+        }
+        if (!cashService.payByBlik(123456)) {
+            throw new RuntimeException(BLIK_PAYMENT_ERROR_MESSAGE);
+        }
+
+        return new Beer(BEER_BRAND);
+    }
+
 }
